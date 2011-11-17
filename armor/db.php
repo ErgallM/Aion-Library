1643488642d8cf1d8323226579ffce55cdc348a8
ErgallM <eaglemoor@gmail.com>
2011-11-17 21:36:20 +0400

    ZF

--- armor/db.php
+++ armor/db.php
@@ -3,9 +3,8 @@ set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__DIR__) . '/libr
 $t = (!empty($_GET['t'])) ? (string) $_GET['t'] : null;
 //if (null == $t) exit();
 
-
-require_once '../library/Zend/Db/Table.php';
-require_once '../library/Zend/Db/Adapter/Pdo/Mysql.php';
+require_once 'ZF.php';
+require_once 'item.php';
 
 Zend_Db_Table::setDefaultAdapter(new Zend_Db_Adapter_Pdo_Mysql(array(
     'host' => 'localhost',
@@ -15,3 +14,9 @@ Zend_Db_Table::setDefaultAdapter(new Zend_Db_Adapter_Pdo_Mysql(array(
     'dbname' => 'aion_library'
 )));
 
+if ('add' == $t) {
+
+    $table = new Model_Item();
+    echo $table->addItem($_POST);
+
+}
---
+++ armor/item.php
@@ -0,0 +1,15 @@
+<?php
+class Model_Item extends Zend_Db_Table
+{
+    protected $_name = 'items';
+    protected $_cols = array('id', 'name', 'lvl', 'type', 'slot', 'q', 'skills', 'manastoneLvl', 'manastoneCount', 'godstone', 'price', 'icon', 'image');
+
+    public function addItem($data)
+    {
+        $data['skills'] = serialize($data['skills']);
+        $data['price'] = serialize($data['price']);
+
+        $row = $this->createRow($data);
+        return $row->save();
+    }
+}
